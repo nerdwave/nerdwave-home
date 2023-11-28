e8949a05d02e017e3a8c66c11acbe2f4ad678fcf
nerdwave-nick <nick@nerdwave.io>
2023-11-28 20:04:19 +0000

    feat: initial nerdwave draft lol

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    <a href="https://nerdwave.io/" target="_blank">
+      <img src="./assets/nerdwave-text.svg" class="logo" alt="Nerdwave logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld />
 </template>
 
 <style scoped>
@@ -22,9 +19,9 @@ import HelloWorld from './components/HelloWorld.vue'
   transition: filter 300ms;
 }
 .logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+  filter: drop-shadow(0 0 1.5em #de38ff);
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.logo {
+  filter: drop-shadow(0 0 1em #b038ff);
 }
 </style>
